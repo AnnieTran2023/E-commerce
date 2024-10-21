757e42d6c5966bbfb9b0279129561a92470f99cd
Annie Tran <tranthilananh105@gmail.com>
2024-10-21 15:40:03 +0300

    implement security config and swagger

--- src/main/java/fi/haagahelia/ecommerce/EcommerceApplication.java
+++ src/main/java/fi/haagahelia/ecommerce/EcommerceApplication.java
@@ -7,8 +7,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import fi.haagahelia.ecommerce.domain.ProductRepository;
+import fi.haagahelia.ecommerce.domain.User;
+import fi.haagahelia.ecommerce.domain.UserRepository;
 import fi.haagahelia.ecommerce.domain.Category;
 import fi.haagahelia.ecommerce.domain.CategoryRepository;
 import fi.haagahelia.ecommerce.domain.Order;
@@ -26,17 +29,23 @@ public class EcommerceApplication {
 			return products.stream().mapToDouble(Product::getPrice).sum();
 		}
 	
-
 	@Bean
-	public CommandLineRunner bookDemo(ProductRepository repository, CategoryRepository categoryRepository, OrderRepository orderRepository) {
+	public CommandLineRunner bookDemo(ProductRepository repository, CategoryRepository categoryRepository, OrderRepository orderRepository, UserRepository userRepository, PasswordEncoder passwordEncoder) {
 		return (args) -> {
-			//Create categories
+			//Create user
+			User user = new User("user1", passwordEncoder.encode("password"), "USER");
+			userRepository.save(user);
+
+			User admin = new User("admin1", passwordEncoder.encode("password"), "ADMIN");
+			userRepository.save(admin);
+
+			// Create categories
 			Category phone = new Category("Phone", "mobile phones");
 			Category tablet = new Category("Tablet", "all sorts of tablets");
 			categoryRepository.save(phone);
 			categoryRepository.save(tablet);
 
-			//Create products
+			// Create products
 			Product iphone15 = new Product("iPhone 15", "The latest iPhone", "https://www.apple.com/newsroom/images/product/iphone/standard/Apple_announce-iphone13pro_09142021_big.jpg.og.jpg", 999.99, phone);
 			Product samsungGalaxy = new Product("Samsung Galaxy", "The latest Samsung Galaxy", "https://www.samsung.com/us/smartphones/galaxy-s21-5g/_jcr_content/og-v2.jpg", 899.99, phone);
 			Product iPad = new Product("iPad", "The latest iPad", "https://store.store", 499.99, tablet);
@@ -47,7 +56,7 @@ public class EcommerceApplication {
 
 			System.out.println("Sample products inserted into the database.");
 
-			 // Create orders
+			// Create orders
             Order order1 = new Order("2023-10-01", 0, "Complete", Arrays.asList(iphone15, samsungGalaxy));
             order1.setTotalPrice(calculateTotalPrice(order1.getProducts()));
             Order order2 = new Order("2023-10-02", 0, "Complete", Arrays.asList(iPad));
--- src/main/java/fi/haagahelia/ecommerce/config/SecurityConfig.java
+++ src/main/java/fi/haagahelia/ecommerce/config/SecurityConfig.java
@@ -1,31 +1,27 @@
 package fi.haagahelia.ecommerce.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import fi.haagahelia.ecommerce.service.CustomUserDetailsService;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Autowired
-    private CustomUserDetailsService userDetailsService;
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(requests -> requests
-                        .antMatchers("/orders/**").hasRole("ADMIN")
-                        .antMatchers("/products/**").hasRole("USER")
-                        .antMatchers("/").permitAll())
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/orders/**").hasRole("ADMIN")
+                        .requestMatchers("/products/**").hasRole("USER")
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .requestMatchers("/").permitAll())
+                .formLogin(withDefaults())
                 .logout(logout -> logout
                         .logoutSuccessUrl("/"));
         return http.build();
@@ -34,10 +30,5 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
 }
