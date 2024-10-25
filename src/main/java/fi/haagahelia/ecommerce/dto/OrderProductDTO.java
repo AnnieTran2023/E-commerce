d1e8e510c0ba058dd693bbdd4fbdcf310ef3d56b
Annie Tran <tranthilananh105@gmail.com>
2024-10-25 23:40:52 +0300

    update OrderProduct with user

--- src/main/java/fi/haagahelia/ecommerce/dto/OrderProductDTO.java
+++ src/main/java/fi/haagahelia/ecommerce/dto/OrderProductDTO.java
@@ -6,15 +6,20 @@ public class OrderProductDTO {
     private String productId;
     private String productName;
     private int quantity;
+    private String userId;
+    private String username;
 
     public OrderProductDTO() {
     }
 
-    public OrderProductDTO(String id, String productId, String productName, int quantity) {
+    public OrderProductDTO(String id, String productId, String productName, int quantity, String userId,
+            String username) {
         this.id = id;
         this.productId = productId;
         this.productName = productName;
         this.quantity = quantity;
+        this.userId = userId;
+        this.username = username;
     }
 
     public String getId() {
@@ -49,4 +54,19 @@ public class OrderProductDTO {
         this.quantity = quantity;
     }
 
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
